d5765b003a6343a1b20cb951bbbc9d76d048044f
onocy <michalowski.phillip@gmail.com>
2024-02-21 02:10:44 -0500

    feat: add super basic profile page

---
+++ pages/profile/index.tsx
@@ -0,0 +1,10 @@
+import { useUser } from "contexts/userContext";
+import React from "react";
+
+const Profile: React.FC = () => {
+  const { user } = useUser();
+
+  return <div>{user.email}</div>;
+};
+
+export default Profile;
