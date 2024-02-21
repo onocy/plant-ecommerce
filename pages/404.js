8582442c30b9816b4ddcc72ad9ca4214e2113142
onocy <michalowski.phillip@gmail.com>
2024-02-21 02:10:28 -0500

    feat: slight tweak of 404

--- pages/404.js
+++ pages/404.js
@@ -5,10 +5,9 @@ const ErrorPage = () => {
     <main>
       <div class="flex justify-center items-center">
         <div class="centered text-center">
-          <Image src="/images/404_final.png" width="800" height="200" alt="" />
-          <div className="font-bold text-xl">
+          <Image src="/images/404_final.png" width="400" height="200" alt="" />
+          <div className="text-xl italic">
             <div>This page must have been overwatered...</div>
-            <div>Don&lsquo;t leaf us just yet!</div>
           </div>
         </div>
       </div>
